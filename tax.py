f9199816079a0640d210a443dff02c9d74637a6e
TobyBradshaw <116697368+TobyBradshaw@users.noreply.github.com>
2022-10-26 14:35:17 +1100

    Update tax.py

--- tax.py
+++ tax.py
@@ -13,12 +13,12 @@ a = st.number_input("Enter a number")
 b = st.number_input("Enter another number")
 operation = st.selectbox("Select Operation", ["Add", "Subtract", "Multiply", "Divide"])
 if operation == "Add":
-st.write(add(a, b))
-elif operation == "Subtract":
-st.write(sub(a, b))
-elif operation == "Multiply":
-st.write(mul(a, b))
-elif operation == "Divide":
-st.write(div(a, b))
-if __name__ == '__main__':
+  st.write(add(a, b))
+  elif operation == "Subtract":
+  st.write(sub(a, b))
+  elif operation == "Multiply":
+  st.write(mul(a, b))
+  elif operation == "Divide":
+  st.write(div(a, b))
+  if __name__ == '__main__':
 main()
