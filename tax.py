421819893b4e564c033145d6717b82de8a61925d
TobyBradshaw <116697368+TobyBradshaw@users.noreply.github.com>
2022-10-26 14:38:01 +1100

    Update tax.py

--- tax.py
+++ tax.py
@@ -1,24 +1,53 @@
-def add(a, b):
-  return a + b
-def sub(a, b):
-  return a - b
-def mul(a, b):
-  return a * b
-def div(a, b):
-  return a / b
+import streamlit as st
+
+
+def get_value(user_expr):
+    """
+    We use eval() to get the value of the expression.
+    ref: https://docs.python.org/3/library/functions.html#eval
+    """
+    return eval(user_expr, {"__builtins__": None})
+
+
+def valid_expression(strex):
+    """
+    Evaluate if strex is a valid mathematical expression.
+    """
+    for i in range(len(strex)):
+        # If the first char is invalid.
+        if i==0:
+            if strex[i] in ['+', '-', '*', '/', '%', ')']:
+                return False
+
+        # If the last char is also invalid.
+        if i == len(strex) - 1:
+            if strex[i] in ['+', '-', '*', '/', '%', '(']:
+                return False
+
+        # Validate char if it is a number, operators or parenthesis.
+        if (strex[i] >= '0' and strex[i] <= '9') or strex[i] in ['+', '-', '*', '/', '%', '(', ')']:
+            continue  # continue with the next character
+        else:
+            return False
+    
+    return True  # We cannot find invalid chars.
+
+    
 def main():
-st.title("Simple Calculator")
-st.write("This is a simple calculator app")
-a = st.number_input("Enter a number")
-b = st.number_input("Enter another number")
-operation = st.selectbox("Select Operation", ["Add", "Subtract", "Multiply", "Divide"])
-if operation == "Add":
-  st.write(add(a, b))
-  elif operation == "Subtract":
-  st.write(sub(a, b))
-  elif operation == "Multiply":
-  st.write(mul(a, b))
-  elif operation == "Divide":
-  st.write(div(a, b))
-  if __name__ == '__main__':
+    st.title(' ofir Calculator')
+
+    # The input value from the user will be saved in user_expr.
+    user_expr = st.text_input('Please enter a mathematical expression')
+
+    if not valid_expression(user_expr):
+        st.error("Invalid Expression")
+    else:
+        st.success("Valid Expression")
+
+        # Get the value of the expression.
+        value = get_value(user_expr)
+        st.write(f'The answer is {value}.')  # ref.: https://docs.python.org/3/tutorial/inputoutput.html
+
+
+# Entry point
 main()
